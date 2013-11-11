2c16922a54f1e8a0517910bb73be7ffa0084d66b
Aaron Brenner <abrenner@pitt.edu>
2013-11-11 11:22:59 -0500

    more verbose when purging

--- drlrepo/ingest/purge_object.py
+++ drlrepo/ingest/purge_object.py
@@ -9,10 +9,10 @@ def purge_item(item_id):
     objs = repo.find_objects(pid__contains=pid)
     for o in objs:
         repo.purge_object(o.pid)
+        print '%s purged' % (o.pid,)
 
 if __name__ == '__main__':
     item_id = sys.argv[1]
-    print 'working with %s' % (item_id,)
+    print 'purging objects with pids containing %s' % (item_id,)
     purge_item(item_id)
-    print '%s purged' % (item_id,)
     
