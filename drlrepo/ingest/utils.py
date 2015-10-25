f4a9a4e7b64854caeb64debfb7b26b21d209bccc
Aaron Brenner <abrenner@pitt.edu>
2015-10-25 09:05:33 -0400

    added relationship convenience functions

--- drlrepo/ingest/utils.py
+++ drlrepo/ingest/utils.py
@@ -9,6 +9,7 @@ import drlutils.image.utils
 import drlrepo.ingest.config
 import logging
 import urllib2
+import requests
 os.environ["DJANGO_SETTINGS_MODULE"] = 'drlrepo.settings'
 from eulfedora.server import Repository
 from eulxml.xmlmap.dc import DublinCore
@@ -24,7 +25,6 @@ For each object, determine content
 type and route to a handler function that will create the object in 
 Fedora and upload all relevant datastreams.
 """
-
 
 # map of legacy item types to eulfedora digital object models 
 ITEM_TYPE_CM_MAP = {
@@ -59,17 +59,41 @@ def add_datastream(pid, this_type, ds_id, content, mimetype=None):
     obj.save()
 
 
-def add_relationship_site(obj, site_obj):
-    isMemberOfSite = 'http://digital.library.pitt.edu/ontology/relations#isMemberOfSite'
-    parent_uri = "info:fedora/%s" % (site_obj.pid,)
-    obj.add_relationship(isMemberOfSite, str(parent_uri))
+def add_relationship_site(obj_pid, site_obj_pid):
+    repo = Repository()
+    obj = repo.get_object(pid=obj_pid)
+    parent_uri = "info:fedora/%s" % (site_obj_pid,)
+    obj.add_relationship(drlrepo.ingest.config.IS_MEMBER_OF_SITE, str(parent_uri))
     obj.save()
 
-def add_relationship_collection(obj, collection_obj):
-    isMemberOfCollection = 'info:fedora/fedora-system:def/relations-external#isMemberOfCollection'
-    parent_uri = "info:fedora/%s" % (collection_obj.pid,)
-    obj.add_relationship(isMemberOfCollection, str(parent_uri))
+def remove_relationship_site(obj_pid, site_obj_pid):
+    # eulfedora does not provide a remove relationship method, so direct API call
+    rel_subject = u'info:fedora/' +  obj_pid
+    rel_predicate = drlrepo.ingest.config.IS_MEMBER_OF_SITE.replace('#', '%23') 
+    rel_object = u'info:fedora/' +  site_obj_pid
+    api_call_url = '%s/fedora/objects/%s/relationships?subject=%s&predicate=%s&object=%s&isLiteral=false' % (drlrepo.ingest.config.FEDORA_URL, obj_pid, rel_subject, rel_predicate, rel_object)
+    r = requests.delete(api_call_url, auth=(drlrepo.ingest.config.FEDORA_USER, drlrepo.ingest.config.FEDORA_PASS))
+    if not r.status_code == requests.codes.ok:
+        return 1
+    return
+
+
+def add_relationship_collection(obj_pid, collection_obj_pid):
+    repo = Repository()
+    obj = repo.get_object(pid=obj_pid)
+    parent_uri = "info:fedora/%s" % (collection_obj_pid,)
+    obj.add_relationship(drlrepo.ingest.config.IS_MEMBER_OF_COLLECTION, str(parent_uri))
     obj.save()
+
+def remove_relationship_collection(obj_pid, collection_obj_pid):
+    rel_subject = u'info:fedora/' +  obj_id
+    rel_predicate = drlrepo.ingest.config.IS_MEMBER_OF_COLLECTION.replace('#', '%23') 
+    rel_object = u'info:fedora/' +  collection_obj_pid
+    api_call_url = '%s/fedora/objects/%s/relationships?subject=%s&predicate=%s&object=%s&isLiteral=false' % (drlrepo.ingest.config.FEDORA_URL, obj_pid, rel_subject, rel_predicate, rel_object)
+    r = requests.delete(api_call_url, auth=(drlrepo.ingest.config.FEDORA_USER, drlrepo.ingest.config.FEDORA_PASS))
+    if not r.status_code == requests.codes.ok:
+        return 1
+    return
         
 def get_image_dimensions(image_path):
     # use exiftool to get image dimensions for specified image
@@ -99,7 +123,7 @@ def get_item_metadata(bag_path):
 
 
 def get_item_metadata_from_url(item_id):
-    url = 'http://bigfoot.library.pitt.edu/django/workflow/item/%s/json/' % (item_id,)
+    url = '%s/django/workflow/item/%s/json/' % (drlrepo.ingest.config.LEGACY_WORKFLOW_URL, item_id,)
     try:
         return json.loads(urllib2.urlopen(url).read())
     except:
@@ -181,10 +205,13 @@ def get_jp2_name(m, t):
     return None
 
 def get_page_thumb_name(m, t):
-    expected_page_thumb_name = '%s.%s' % (os.path.splitext(t)[0], 'thumb.jpg') 
+    expected_page_thumb_name_jpg = '%s.%s' % (os.path.splitext(t)[0], 'thumb.jpg') 
+    expected_page_thumb_name_png = '%s.%s' % (os.path.splitext(t)[0], 'thumb.png') 
     for f in m['files']:
         if m['files'][f]['use'] == 'PAGE_THUMB':
-            if m['files'][f]['name'] == expected_page_thumb_name:
+            if m['files'][f]['name'] == expected_page_thumb_name_jpg:
+                return m['files'][f]['name']
+            elif m['files'][f]['name'] == expected_page_thumb_name_png:
                 return m['files'][f]['name']
     return None
 
